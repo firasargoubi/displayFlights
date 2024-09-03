ffb1f194dfdbc438ea38f0376787ded8089b6d6e
Firas Argoubi <99605613+firasargoubi@users.noreply.github.com>
2024-09-02 23:36:04 -0400

    Update settings.py

--- flightdatapipeline/settings.py
+++ flightdatapipeline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7mjy@f*$^8txs=ah!7$4hvn8bgj*3yqeo-5*3*#ysl4$e4ka5!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["displayflights.onrender.com"]
 
 
 # Application definition
